e1b18e59dea32eba88f7f3535f568070511ae8be
Ali Esmaili <AESM85@gmail.com>
2015-04-21 04:11:39 -0700

    SMACSS partials

---
+++ public/scss/modules/configure.rb
@@ -0,0 +1,10 @@
+# configure.rb
+
+http_path = "/"
+css_dir = "."
+sass_dir = "."
+images_dir = "images"
+javascripts_dir = "js"
+output_style = :compressed
+relative_assets = true
+line_comments = false
