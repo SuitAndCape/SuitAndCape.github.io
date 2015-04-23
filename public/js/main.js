4498464f0a00451bd0b06248a3c34cab9d293955
Ali Esmaili <AESM85@gmail.com>
2015-04-23 14:10:46 -0700

    Include sidebar skeleton, add sass-cache to gitignore

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,10 @@
 // main.js
 
-///// HELLO
+///// MAIN JAVASCRIPT SNIPPETS
 //=======================================================================/
 console.log("Thank\'s for visiting my website!  Please say hi and let me know what you think.  Yay!");
+
+/*===== Navigation bar collapser =====*/
+$("#toggle").click(function() {
+  $(this).next(".nav").toggleClass("is-collapsed-mobile");
+});
